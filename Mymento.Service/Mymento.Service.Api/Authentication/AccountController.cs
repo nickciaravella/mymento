ff191008dc5920fcc91b342eccf05d6ac3b00d9a
Nick Ciaravella <nick.ciaravella@live.com>
2016-10-19 20:58:37 -0700

    Added registration code

---
+++ Mymento.Service/Mymento.Service.Api/Authentication/AccountController.cs
@@ -0,0 +1,72 @@
+﻿namespace Mymento.Service.Api.Authentication
+{
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Microsoft.AspNet.Identity;
+    using Mymento.Service.Api.Models;
+
+    [RoutePrefix("api/accounts")]
+    public class AccountController : ApiController
+    {
+        private readonly AuthRepository _repo = null;
+
+        public AccountController()
+        {
+            _repo = new AuthRepository();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IHttpActionResult> PostAccount(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await _repo.RegisterUserAsync(userModel);
+
+            IHttpActionResult errorResult = GetErrorResult(result);
+            return errorResult ?? Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Mymento.Service/Mymento.Service.Api/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Mymento.Service.Api.Models
+{
+    public class UserModel
+    {
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+    }
+}
